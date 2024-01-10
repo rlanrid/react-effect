936a2f5fdb5b5cbe1368ad7d1426a4d75e7bd211
rlanrid <rladnrider77@gmail.com>
2024-01-10 16:04:29 +0900

    첫 셋팅

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer id='react__footer'>
+            rladnrider77@gmail.com
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = ({ children }) => {
+    return (
+        <main id='react__main'>
+            {children}
+        </main>
+    )
+}
+
+export default Main
